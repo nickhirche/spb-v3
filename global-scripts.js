fce200f5747c0584dffcedd10d25b7bd66b14bfa
Nick Hirche <nick.hirche@ueber.io>
2023-01-14 22:32:24 +0100

    Script js

---
+++ global-scripts.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper('.swiper-plays-slider', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 0,
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 480px
+        600: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+        },
+        // when window width is >= 640px
+        960: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+        },
+    },
+    // Navigation
+    navigation: {
+        nextEl: ".swiper-button-next.plays",
+        prevEl: ".swiper-button-prev.plays",
+    },
+});
+    var swiper = new Swiper('.swiper-story-slider', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+        loopedSlides: 6,
+    speed: 400,
+    
+    autoplay: {
+        delay: 2500,
+        pauseOnMouseEnter: true,
+                disableOnInteraction: false,
+        disableOnInteraction: false,
+    },
+});
